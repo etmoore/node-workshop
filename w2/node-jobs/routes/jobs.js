edd9d6acd3ca4138c8eb8da1fb704615176bf5a1
Evan Moore <etmoore@gmail.com>
2017-03-02 20:57:26 -0700

    add job index, update, and new routes

--- w2/node-jobs/routes/jobs.js
+++ w2/node-jobs/routes/jobs.js
@@ -2,23 +2,40 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models.js');
 
+/*** jobs index ***/
 router.get('/', (req, res, next) => {
-  res.render("jobs/index.html", {
-    title: "All Jobs",
+  res.render('jobs/index.html', {
+    title: 'All Jobs',
     jobs: model.getAllJobs(),
+  });
+});
+
+/*** new job***/
+router.get('/new', (req, res, next) => {
+  res.render('jobs/new.html', {
+    title: 'New Job',
   });
 });
 
 router.post('/', (req, res, next) => {
   model.createNewJob(req.body);
-  res.redirect("/jobs");
+  res.redirect('/jobs');
 });
 
-
-router.get('/new', (req, res, next) => {
-  res.render("jobs/new.html", {
-    title: "New Job",
+/*** update job ***/
+router.get('/:id/update', (req, res, next) => {
+  let id = parseInt(req.params.id);
+  let job = model.getJob(id);
+  res.render('jobs/update.html', {
+    title: 'Update Job',
+    job
   });
+});
+
+router.post('/:id', (req, res, next) => {
+  let id = parseInt(req.params.id);
+  model.updateJob(req.body, id);
+  res.redirect('/jobs');
 });
 
 
