2908dac1bd982201d26aed32e45ecda2b2b3b6a7
Evan Moore <etmoore@gmail.com>
2017-03-06 12:38:11 -0700

    fix serialize bug by correcting modal and form ids

--- w2/node-jobs-client/helpers.js
+++ w2/node-jobs-client/helpers.js
@@ -2,17 +2,29 @@ function getAllJobs() {
   return $.getJSON('http://localhost:3000/');
 }
 
+function getJob(id) {
+  return $.ajax({
+    method: 'GET',
+    url: `http://localhost:3000/${id}`,
+  });
+}
+
 function addJob(dataString) {
-  console.log(dataString);
   return $.ajax({
     method: 'POST',
     url: 'http://localhost:3000/',
-    datatype: 'json',
     data: dataString,
-  })
+  });
 }
 
-function updateJob() {}
+function updateJob(id, dataString) {
+  return $.ajax({
+    method: 'PUT',
+    url: `http://localhost:3000/${id}`,
+    datatype: 'json',
+    data: dataString,
+  });
+}
 
 function deleteJob(id) {
   return $.ajax(`http://localhost:3000/${id}`, {
--- w2/node-jobs-client/main.js
+++ w2/node-jobs-client/main.js
@@ -14,19 +14,52 @@ $('body').on('click', '.delete-job', function(){
     .fail(err => console.log(err));
 });
 
-$('.modal').on('submit', 'form', function(e){
+$('#new-job-modal').on('submit', '#new-job-form', function(e){
   e.preventDefault();
-  let dataString = $('form').serialize();
+  let dataString = $('#new-job-form').serialize();
+  console.log("datastring: ", dataString)
 
   addJob(dataString)
     .then(() => {
       $('form').find("input, textarea").val("");
-      $('#contacted').prop('checked', false);
+      $('#job-contacted').prop('checked', false);
       $('tbody').html('');
     })
     .then(getAllJobs)
     .then(buildJobsTable)
-    .then(() => $('#myModal').modal('toggle'))
+    .then(() => $('#new-job-modal').modal('toggle'))
+    .catch(console.error);
+});
+
+$('body').on('click', '.update-job', function(){
+  let id = $(this).data('id');
+  getJob(id)
+    .then(job => {
+      $("#job-id").val(job.id);
+      $("#job-title").val(job.title);
+      $("#job-description").val(job.description);
+      $("#job-company").val(job.company);
+      $("#job-email").val(job.email);
+      $("#job-contacted").prop('checked', job.contacted);
+    })
+    .catch(console.error);
+});
+
+$('#update-job-modal').on('submit', '#update-job-form', function(e){
+  e.preventDefault();
+
+  let id = $("#job-id").val();
+  let dataString = $('form').serialize();
+
+  updateJob(id, dataString)
+    .then(() => {
+      $('form').find("input, textarea").val("");
+      $('#job-contacted').prop('checked', false);
+      $('tbody').html('');
+    })
+    .then(getAllJobs)
+    .then(buildJobsTable)
+    .then(() => $('#update-job-modal').modal('toggle'))
     .catch(err => console.log(err));
 });
 
@@ -40,7 +73,7 @@ function buildJobsTable(jobs){
           <td>${job.company}</td>
           <td>${job.email}</td>
           <td>${job.contacted ? "yes" : "no"}</td>
-          <td><a class="btn btn-warning" href="jobs/${job.id}/update">Update</a></td>
+          <td><a class="update-job btn btn-warning" data-id=${job.id} data-toggle="modal" data-target="#updateJobModal">Update</a></td>
           <td><a class="delete-job btn btn-danger" data-id=${job.id}>Delete</a></td>
         </tr>`
     );
