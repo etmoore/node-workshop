e79e469046223bde6725a1557d86f2c63a47af11
Evan Moore <etmoore@gmail.com>
2017-03-07 17:14:51 -0700

    add migration to create show table

---
+++ w2/test-driven-node/mocha-chai-knex/db/knex.js
@@ -0,0 +1,4 @@
+var environment = process.env.NODE_ENV || 'development'; // NODE_ENV is usually set in express
+var config = require('../knexfile.js')[environment]; // grabs the appropriate db settings from knexfile.js based on the environment variable set above
+
+module.exports = require('knex')(config);
