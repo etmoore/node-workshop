7888ba863f70b844801e0d0eecb2c155a244186d
Evan Moore <etmoore@gmail.com>
2017-03-09 14:09:50 -0700

    configure eslint

--- w2/react-intro/gulpfile.js
+++ w2/react-intro/gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
-gulp.task('build', () => {
+gulp.task('build', ['lint'], () => {
   gulp.src(['src/**/*.js']) // 'gulp!' the javascript files to be transpiled
     .pipe(babel()) // run the code through babel transpiler
     .pipe(gulp.dest('lib')); // write the transpiled code to the lib folder
+});
+
+gulp.task('lint', () => {
+  gulp.src([ // gulp the gulpfile and any js files in src
+    'gulpfile.js',
+    'src/**/*js',
+  ])
+    .pipe(eslint()) // run it through eslint linter
+    .pipe(eslint.format()) // outputs the lint results to the console
+    .pipe(eslint.failAfterError()); // stop if any errors have been found
 });
