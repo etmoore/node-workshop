c9668488a8ef0d616888a57fbf39082846469d52
Evan Moore <etmoore@gmail.com>
2017-03-19 15:01:19 -0600

    show form, hide table when click add job. also add console.log when you submit the new job form

--- w2/node-jobs-react/src/components/JobList.jsx
+++ w2/node-jobs-react/src/components/JobList.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 
 const JobList = (props) => {
+  const { jobs } = props;
   return (
-    <table className="table table-hover">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Company</th>
-        <th>Email</th>
-        <th>Contacted?</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        props.jobs.map((job) => {
-          return <tr key={job.id}>
-          <td>{ job.id }</td>
-          <td>{ job.title }</td>
-          <td>{ job.description }</td>
-          <td>{ job.company }</td>
-          <td>{ job.email }</td>
-          <td>{ job.contacted ? 'yes' : 'no' }</td>
-          </tr>
-        })
-      }
-    </tbody>
-  </table>
+    <div>
+      <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Company</th>
+          <th>Email</th>
+          <th>Contacted?</th>
+        </tr>
+      </thead>
+        <tbody>
+          {
+            jobs.map((job) => {
+              return (
+                <tr key={job.id}>
+                  <td>{ job.id }</td>
+                  <td>{ job.title }</td>
+                  <td>{ job.description }</td>
+                  <td>{ job.company }</td>
+                  <td>{ job.email }</td>
+                  <td>{ job.contacted ? 'yes' : 'no' }</td>
+                  <td><button className="btn btn-default">Update Job</button></td>
+                  <td><button className="btn btn-danger">Delete Job</button></td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- w2/node-jobs-react/src/index.js
+++ w2/node-jobs-react/src/index.js
@@ -9,11 +9,17 @@ class App extends Component {
     super()
     this.state = {
       jobs: [],
+      showForm: false,
     }
   }
 
-  componentDidMount(){
+  componentWillMount(){
     this.getJobs()
+  }
+
+  addJob(e) {
+    e.preventDefault();
+    console.log('yeah! you added a job');
   }
 
   getJobs() {
@@ -26,11 +32,50 @@ class App extends Component {
       .catch((err) => { console.log(err); })
   }
 
+  toggleShowForm() {
+    this.setState({
+      showForm: !this.state.showForm,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-      <h1>Node Jobs</h1>
-      <JobList jobs={this.state.jobs}/>
+        <h1>Node Jobs</h1>
+        <button className="btn btn-primary" onClick={() => this.toggleShowForm()}>{ this.state.showForm ? 'Cancel' : 'Add Job' }</button>
+
+        { this.state.showForm &&
+          <form onSubmit={ (e) => this.addJob(e) }>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input type="text" name="title" className="form-control" id="title" defaultValue="job title"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea className="form-control" rows="3" id="description" defaultValue="this is a description"></textarea>
+            </div>
+            <div className="form-group">
+              <label htmlFor="company">Company</label>
+              <input type="text" name="company" className="form-control" id="company" defaultValue="job title"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" className="form-control" id="email" defaultValue="email@example.com"/>
+            </div>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" name="contacted"/> Contacted?
+              </label>
+            </div>
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+        }
+
+        { !this.state.showForm &&
+          <JobList
+            jobs={ this.state.jobs }
+            toggleShowForm={this.toggleShowForm.bind(this)} />
+        }
       </div>
     )
   }
