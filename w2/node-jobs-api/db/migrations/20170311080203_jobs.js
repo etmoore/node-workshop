2e9a93ccb3d41773ddc94cc420623221605ed8a4
Evan Moore <etmoore@gmail.com>
2017-03-23 16:16:44 -0600

    remove extraneous comment slashes from migration file

--- w2/node-jobs-api/db/migrations/20170311080203_jobs.js
+++ w2/node-jobs-api/db/migrations/20170311080203_jobs.js
@@ -1,11 +1,11 @@
 exports.up = knex => (
   knex.schema.createTable('jobs', (table) => {
-    table.increments('id');
-    table.string('title').notNullable(); //
-    table.string('description').notNullable(); //
-    table.string('company').notNullable(); //
-    table.string('email').notNullable(); //
-    table.boolean('contacted').notNullable().defaultTo(false); //
+    table.increments('id'); // auto-incrementing integer
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.string('company').notNullable();
+    table.string('email').notNullable();
+    table.boolean('contacted').notNullable().defaultTo(false);
     table.timestamps(true, true);
   })
 );
